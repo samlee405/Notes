c675e7d7586509fec210bec318304b930f35261d
Samuel Lee <samlee405@yahoo.com>
2016-06-27 12:16:41 -0700

    Completed Notes App

---
+++ MakeSchoolNotes/Models/Notes.swift
@@ -0,0 +1,17 @@
+//
+//  Notes.swift
+//  MakeSchoolNotes
+//
+//  Created by Sam Lee on 6/23/16.
+//  Copyright © 2016 MakeSchool. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Note: Object {
+    dynamic var title = ""
+    dynamic var content = ""
+    dynamic var modificationTime = NSDate()
+}
+
